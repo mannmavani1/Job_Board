d7d9680d29929a614e202b9ca24f17eec9c7b6ac
Mann Mavani <mannm@zignuts.com>
2026-01-12 10:09:23 +0530

    Implemented Middleware and Changed Location of main.py from app folder to root.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import logging
+import time
+from fastapi import FastAPI, Request
 from sqlmodel import SQLModel, text
 from app.database.db import engine
 from app.routers.auth import router as auth_router
@@ -6,9 +8,51 @@ from app.routers.jobs import router as jobs_router
 from app.routers.companies import router as companies_router
 from app.routers.applications import router as applications_router
 from app.routers.tag import router as tag_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="Job Board API")
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)   
+
+@app.middleware("http")
+async def log_middleware(request: Request, call_next):
+    start_time = time.time()
+    
+  
+    logger.info(f"Incoming Request: {request.method} {request.url}")
+    
+    
+    try:
+        response = await call_next(request)
+    except Exception as e:
+       
+        logger.error(f"Request failed: {e}")
+        raise e
+    
+   
+    process_time = time.time() - start_time
+    
+   
+    logger.info(
+        f"Completed: {response.status_code} "
+        f"| Time: {process_time:.4f}s"
+    )
+    
+    return response
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       
+    allow_credentials=True,      
+    allow_methods=["*"],         
+    allow_headers=["*"],        
+)
+
 
 @app.on_event("startup")
 def on_startup():
